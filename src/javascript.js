4d4236134fb7ae5e2b6959b3671c6178a041e6f3
Sam <samuel-denomme@hotmail.com>
2023-10-15 21:35:27 +0200

    fix:final

--- src/javascript.js
+++ src/javascript.js
@@ -20,3 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   });
  
+//je vais devoir séparer les deux pour les gérer séparément car si j'importe pas un
+//ou l'autre, un erreur sera déclarer dans la console, et ça, c'est pas propre, exemple,
+//comme ce qui ce produit dans la console du CGU.html
