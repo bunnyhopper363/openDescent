ec4d74a33d519f114e53c6016ffd1e0f92813b38
Sam <samuel-denomme@hotmail.com>
2023-10-24 16:33:02 +0200

    fix: recup des equipes depuis la BDD

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = 3000; // Choisissez un port pour votre serveur
 
 app.use(express.json());
 
+// Importez la configuration de la base de données
+const db = require('./db-config');
+
 // Route pour gérer la sélection du personnage
 app.post('/selection-personnage', (req, res) => {
     const { personnage } = req.body;
@@ -13,12 +16,17 @@ app.post('/selection-personnage', (req, res) => {
 
 // Route pour gérer la récupération des équipes
 app.get('/equipes', (req, res) => {
-    const query = 'SELECT * FROM equipe';
-    const equipes = [
-        { id: 1, nom: 'Les Aventuriers du destion' },
-    ];
+    const query = 'SELECT * FROM team'; // Assurez-vous que la table s'appelle "team" dans votre base de données
 
-    res.json(equipes);
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la récupération des équipes : ' + err);
+            res.status(500).json({ error: 'Erreur lors de la récupération des équipes' });
+            return;
+        }
+
+        res.json(results);
+    });
 });
 
 // Route pour gérer la connexion
