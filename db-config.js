861e2e8977063162d8be172156c672eb2ac27eae
Sam <samuel-denomme@hotmail.com>
2023-10-24 12:07:04 +0200

    mise en place des configuration de db-config.js

--- db-config.js
+++ db-config.js
@@ -1,12 +1,10 @@
-// db-config.js fichier en lien avec la BDD 
-
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'ton_utilisateur',
-  team: 'nom_equipe',
-  database: 'nom_de_ta_base_de_donnees'
+  user: 'opendescent',
+  password: 'opendescent363', 
+  database: 'opendescent'
 });
 
 module.exports = connection;
